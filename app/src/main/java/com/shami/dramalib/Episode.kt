2c8ec3593872c65245a3a78a3327a9b656dd4a4b
Ahtsham25 <princeshami365@gmail.com>
2026-07-30 06:46:37 +0500

    Add Episode data class with properties

---
+++ app/src/main/java/com/shami/dramalib/Episode.kt
@@ -0,0 +1,9 @@
+package com.shami.dramalib
+
+data class Episode(
+    val id: String,
+    val title: String,
+    val videoUrl: String,
+    val downloadUrl: String,
+    val thumbnailUrl: String
+)
